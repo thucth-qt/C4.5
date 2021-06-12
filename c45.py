410f401f42a2e8007072c561590ea4256e01bdaa
thucth <thucth@vng.com.vn>
2021-06-12 16:57:33 +0700

    simplify

--- c45.py
+++ c45.py
@@ -1,204 +1,6 @@
 import math
-class C45:
-
-	"""Creates a decision tree with C4.5 algorithm"""
-	def __init__(self, pathToData,pathToNames):
-		self.filePathToData = pathToData
-		self.filePathToNames = pathToNames
-		self.data = []
-		self.classes = []
-		self.numAttributes = -1 
-		self.attrValues = {}
-		self.attributes = []
-		self.tree = None
-
-	def fetchData(self):
-		with open(self.filePathToNames, "r") as file:
-			classes = file.readline()
-			self.classes = [x.strip() for x in classes.split(",")]
-			#add attributes
-			for line in file:
-				[attribute, values] = [x.strip() for x in line.split(":")]
-				values = [x.strip() for x in values.split(",")]
-				self.attrValues[attribute] = values
-		self.numAttributes = len(self.attrValues.keys())
-		self.attributes = list(self.attrValues.keys())
-		with open(self.filePathToData, "r") as file:
-			for line in file:
-				row = [x.strip() for x in line.split(",")]
-				if row != [] or row != [""]:
-					self.data.append(row)
-
-	def preprocessData(self):
-		'''
-			convert real value into float
-		'''
-		for index,row in enumerate(self.data):
-			for attr_index in range(self.numAttributes):
-				if(not self.isAttrDiscrete(self.attributes[attr_index])):
-					self.data[index][attr_index] = float(self.data[index][attr_index])
-
-	def printTree(self):
-		self.printNode(self.tree)
-
-	def printNode(self, node, indent=""):
-		if not node.isLeaf:
-			if node.threshold is None:
-				#discrete
-				for index,child in enumerate(node.children):
-					if child.isLeaf:
-						print(indent + node.label + " = " + attributes[index] + " : " + child.label)
-					else:
-						print(indent + node.label + " = " + attributes[index] + " : ")
-						self.printNode(child, indent + "	")
-			else:
-				#numerical
-				leftChild = node.children[0]
-				rightChild = node.children[1]
-				if leftChild.isLeaf:
-					print(indent + node.label + " <= " + str(node.threshold) + " : " + leftChild.label)
-				else:
-					print(indent + node.label + " <= " + str(node.threshold)+" : ")
-					self.printNode(leftChild, indent + "	")
-
-				if rightChild.isLeaf:
-					print(indent + node.label + " > " + str(node.threshold) + " : " + rightChild.label)
-				else:
-					print(indent + node.label + " > " + str(node.threshold) + " : ")
-					self.printNode(rightChild , indent + "	")
-
-
-
-	def generateTree(self):
-		self.tree = self.recursiveGenerateTree(self.data, self.attributes)
-
-	def recursiveGenerateTree(self, curData, curAttributes):
-		allSame = self.allSameClass(curData)
-
-		if len(curData) == 0:
-			#Fail
-			return Node(True, "Fail", None)
-		elif allSame is not False:
-			#return a node with that class
-			return Node(True, allSame, None)
-		elif len(curAttributes) == 0:
-			#return a node with the majority class
-			majClass = self.getMajClass(curData)
-			return Node(True, majClass, None)
-		else:
-			(best,best_threshold,splitted) = self.splitAttribute(curData, curAttributes)
-			remainingAttributes = curAttributes[:]
-			remainingAttributes.remove(best)
-			node = Node(False, best, best_threshold)
-			node.children = [self.recursiveGenerateTree(subset, remainingAttributes) for subset in splitted]
-			return node
-
-	def getMajClass(self, curData):
-		freq = [0]*len(self.classes)
-		for row in curData:
-			index = self.classes.index(row[-1])
-			freq[index] += 1
-		maxInd = freq.index(max(freq))
-		return self.classes[maxInd]
-
-
-	def allSameClass(self, data):
-		for row in data:
-			if row[-1] != data[0][-1]:
-				return False
-		return data[0][-1]
-
-	def isAttrDiscrete(self, attribute):
-		if attribute not in self.attributes:
-			raise ValueError("Attribute not listed")
-		elif len(self.attrValues[attribute]) == 1 and self.attrValues[attribute][0] == "continuous":
-			return False
-		else:
-			return True
-
-	def splitAttribute(self, curData, curAttributes):
-		splitted = []
-		maxEnt = -1*float("inf")
-		best_attribute = -1
-		#None for discrete attributes, threshold value for continuous attributes
-		best_threshold = None
-		for attribute in curAttributes:
-			indexOfAttribute = self.attributes.index(attribute)
-			if self.isAttrDiscrete(attribute):
-				#split curData into n-subsets, where n is the number of 
-				#different values of attribute i. Choose the attribute with
-				#the max gain
-				valuesForAttribute = self.attrValues[attribute]
-				subsets = [[] for a in valuesForAttribute]
-				for row in curData:
-					for index in range(len(valuesForAttribute)):
-						if row[i] == valuesForAttribute[index]:
-							subsets[index].append(row)
-							break
-				e = gain(curData, subsets)
-				if e > maxEnt:
-					maxEnt = e
-					splitted = subsets
-					best_attribute = attribute
-					best_threshold = None
-			else:
-				#sort the data according to the column.Then try all 
-				#possible adjacent pairs. Choose the one that 
-				#yields maximum gain
-				curData.sort(key = lambda x: x[indexOfAttribute])
-				for j in range(0, len(curData) - 1):
-					if curData[j][indexOfAttribute] != curData[j+1][indexOfAttribute]:
-						threshold = (curData[j][indexOfAttribute] + curData[j+1][indexOfAttribute]) / 2
-						less = []
-						greater = []
-						for row in curData:
-							if(row[indexOfAttribute] > threshold):
-								greater.append(row)
-							else:
-								less.append(row)
-						e = self.gain(curData, [less, greater])
-						if e >= maxEnt:
-							splitted = [less, greater]
-							maxEnt = e
-							best_attribute = attribute
-							best_threshold = threshold
-		return (best_attribute,best_threshold,splitted)
-
-	def gain(self,unionSet, subsets):
-		#input : data and disjoint subsets of it
-		#output : information gain
-		S = len(unionSet)
-		#calculate impurity before split
-		impurityBeforeSplit = self.entropy(unionSet)
-		#calculate impurity after split
-		weights = [len(subset)/S for subset in subsets]
-		impurityAfterSplit = 0
-		for i in range(len(subsets)):
-			impurityAfterSplit += weights[i]*self.entropy(subsets[i])
-		#calculate total gain
-		totalGain = impurityBeforeSplit - impurityAfterSplit
-		return totalGain
-
-	def entropy(self, dataSet):
-		S = len(dataSet)
-		if S == 0:
-			return 0
-		num_classes = [0 for i in self.classes]
-		for row in dataSet:
-			classIndex = list(self.classes).index(row[-1])
-			num_classes[classIndex] += 1
-		num_classes = [x/S for x in num_classes]
-		ent = 0
-		for num in num_classes:
-			ent += num*self.log(num)
-		return ent*-1
-
-
-	def log(self, x):
-		if x == 0:
-			return 0
-		else:
-			return math.log(x,2)
+import numpy as np
+import pandas as pd
 
 class Node:
 	def __init__(self,isLeaf, label, threshold):
@@ -206,3 +8,126 @@ class Node:
 		self.threshold = threshold
 		self.isLeaf = isLeaf
 		self.children = []
+class C45:
+	"""Creates a bi-decision tree with C4.5 algorithm"""
+	def __init__(self, pathToData):
+		self.filePathToData = pathToData
+		self.data = []
+		self.classes = []
+		self.numAttributes = -1 
+		self.attributes = []
+		self.tree = None
+
+	def load_data(self, attributes:list, class_col):
+		df = pd.read_excel(self.filePathToData, sheet_name=0, index_col=None, header=0, usecols=attributes+[class_col])  
+		self.attributes = attributes
+		self.data = df.values.tolist()
+		self.classes = list(set(df.iloc[:,-1]))
+		self.numAttributes = len(attributes)
+
+	def printTree(self):
+		self.printNode(self.tree)
+
+	def printNode(self, node, indent=""):
+		if node.isLeaf: return 
+
+		leftChild = node.children[0]
+		rightChild = node.children[1]
+		if leftChild.isLeaf:
+			print(indent + node.label + " <= " + str(node.threshold) + " : " + leftChild.label)
+		else:
+			print(indent + node.label + " <= " + str(node.threshold)+" : ")
+			self.printNode(leftChild, indent + "	")
+
+		if rightChild.isLeaf:
+			print(indent + node.label + " > " + str(node.threshold) + " : " + rightChild.label)
+		else:
+			print(indent + node.label + " > " + str(node.threshold) + " : ")
+			self.printNode(rightChild , indent + "	")
+
+	def generateTree(self):
+		self.tree = self.recursiveGenerateTree(self.data, self.attributes)
+
+	def recursiveGenerateTree(self, curData, curAttributes):
+		if len(curData) == 0:
+			#No any data sample for this curAttributes. (only in decrete attr)
+			return Node(True, "Fail", None)
+
+		is_pure, class_ = self.allSameClass(curData)
+		if is_pure:
+			return Node(True, class_, None)
+		elif len(curAttributes) == 0:
+			main_class = self.get_main_class(curData)
+			return Node(True, main_class, None)
+		else:
+			(best_attr, threshold, Sis) = self.split_data(curData, curAttributes)
+			remainingAttributes = curAttributes[:]
+			remainingAttributes.remove(best_attr)
+			node = Node(False, best_attr, threshold)
+			node.children = [self.recursiveGenerateTree(Si, remainingAttributes) for Si in Sis]
+			return node
+
+	def get_main_class(self, S):
+		labels = [row[-1] for row in S]
+		classes, count = np.unique(labels, return_counts=True)
+		max_idx = np.argmax(count)
+		return classes[max_idx]
+
+	def allSameClass(self, data):
+		'''
+			Check if all rows is the same class
+			Return:
+				False: different classes
+				Class_name: if all rows are the same class
+		'''
+		for row in data:
+			if row[-1] != data[0][-1]:
+				return False, None
+		return True, data[0][-1]
+
+	def split_data(self, curData, curAttributes):
+		splitted = []
+		maxEnt = -1*float("inf")
+		best_attribute = -1
+		#None for discrete attributes, threshold value for continuous attributes
+		best_threshold = None
+		for attribute in curAttributes:
+			indexOfAttribute = self.attributes.index(attribute)
+	
+			curData.sort(key = lambda x: x[indexOfAttribute])
+			for j in range(0, len(curData) - 1):
+				if curData[j][indexOfAttribute] != curData[j+1][indexOfAttribute]:
+					threshold = (curData[j][indexOfAttribute] + curData[j+1][indexOfAttribute]) / 2
+					less = []
+					greater = []
+					for row in curData:
+						if(row[indexOfAttribute] > threshold):
+							greater.append(row)
+						else:
+							less.append(row)
+					e = self.gain(curData, [less, greater])
+					if e >= maxEnt:
+						splitted = [less, greater]
+						maxEnt = e
+						best_attribute = attribute
+						best_threshold = threshold
+		return (best_attribute,best_threshold,splitted)
+
+	def gain(self,S, Sis):
+
+		E_S = self.entropy(S)
+
+		total_E_Si = sum([len(Si)/len(S)*self.entropy(Si)  for Si in Sis])
+
+		Gain = E_S - total_E_Si
+		return Gain
+
+	def entropy(self, S):
+		labels = [row[-1] for row in S]
+		S = len(labels)
+
+		_,Si = np.unique(labels, return_counts=True)
+		Si = list(Si)
+		entropy = -sum([si_/S * math.log(si_/S,2) for si_ in Si])
+		
+		return entropy
--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-#!/usr/bin/env python
-import pdb
 from c45 import C45
-from my_c45 import MyC45
 
-c1 = C45("data/graduation.csv", "data/attribute.txt")
-c1.fetchData()
-c1.preprocessData()
+c1 = C45("data/All.xlsx")
+c1.load_data(["HK01", "HK02", "HK03", "HK04"], "Graduation")
 c1.generateTree()
 c1.printTree()
-
-
-# c2 = MyC45("data/graduation.xlsx")
-
-
